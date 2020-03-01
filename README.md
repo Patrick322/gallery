6ec9b6a278a4f48c684abce5c9a26274f1d35472
Patrick595 <patrickbuong@gmail.com>
2020-03-01 18:31:28 +0300

    added README.md that shows the information about the application and how it works

---
+++ README.md
@@ -0,0 +1,5 @@
+## Project Title
+GalleryApp
+
+## Project Description
+This website application bassically displays the images posted to the website through admin side and displaying to the user interface.
--- mygallery/models.py
+++ mygallery/models.py
@@ -1,4 +1,4 @@
-class Image(models.Model):
+ jclass Image(models.Model):
     first_name = models.CharField(max_length =30)
     last_name = models.CharField(max_length =30)
     email = models.EmailField()
